ec0858eaae8e85f43f31085ffc97d5e5f6b99f8b
Ben Carter <ben@tearsofaunicorn.com>
2020-04-09 09:10:30 +0800

    updating install script

--- src/main/dist/install.sh
+++ src/main/dist/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 SIF_HOME=/var/sif/hitsprovider
+SITE=`cat ${SIF_HOME}/instance`
 RELEASE_NAME=${project.artifactId}-${project.version}
 FINAL_NAME=SIF3InfraREST
 TOMCAT_HOME=/var/lib/tomcat8
 TIMESTAMP=`date +%Y%m%dT%H%M%S`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
+cd ${DIR}/..
 PROVIDERS=(`find src -name *Provider.java | awk -F '[/.]' '{ print $8 }'`)
-TARGET_URL=`cat ${SIF_HOME}/providers/HitsProvider.properties | grep env.connector.url.secure | grep -v '#' | awk -F '=' '{print $2}'`
-echo Updating Provider environment template
+echo Updating Provider environment template (${SITE})
 cp ${DIR}/../config/environments/provider/template/direct/HITS.xml ${SIF_HOME}/environments/provider/template/direct/HITS.xml
 echo Updating Provider properties
 PROVIDER_CONFIG=""
@@ -23,8 +23,9 @@ for PROVIDER in "${PROVIDERS[@]}"; do
       PREFIX=","
    fi
 done
-sed -i 's/^provider.classes=.*//' ${SIF_HOME}/providers/HitsProvider.properties
+cat ${DIR}/../config/sites/${SITE}/providers/HitsProvider.properties > ${SIF_HOME}/providers/HitsProvider.properties 
 echo "${PROVIDER_CONFIG}" >> ${SIF_HOME}/providers/HitsProvider.properties
+cp ${DIR}/../config/sites/${SITE}/*.* ${SIF_HOME}/
 echo "Installing maven dependencies (required for testing)"
 mvn -q install:install-file -Dfile=dist/sif3-common-${sif3.framework.version}.jar
 mvn -q install:install-file -Dfile=dist/sif3-infra-common-${sif3.framework.version}.jar
