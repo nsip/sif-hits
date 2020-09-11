e95cc87328496e39f393ac6decd71559c1679cfa
Ben Carter <ben@tearsofaunicorn.com>
2020-09-11 11:31:06 +0800

    FQ Test Data

--- src/test/java/sif3/hits/rest/consumer/data/FinancialQuestionnaireCollectionTestData.java
+++ src/test/java/sif3/hits/rest/consumer/data/FinancialQuestionnaireCollectionTestData.java
@@ -1,10 +1,20 @@
 package sif3.hits.rest.consumer.data;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import sif.dd.au30.model.AGRuleListType;
+import sif.dd.au30.model.AGRuleType;
+import sif.dd.au30.model.FQContextualQuestionListType;
+import sif.dd.au30.model.FQContextualQuestionType;
+import sif.dd.au30.model.FQItemListType;
+import sif.dd.au30.model.FQItemType;
+import sif.dd.au30.model.FQReportingListType;
+import sif.dd.au30.model.FQReportingType;
 import sif.dd.au30.model.FinancialQuestionnaireCollectionCollectionType;
 import sif.dd.au30.model.FinancialQuestionnaireCollectionType;
+import sif.dd.au30.model.LocalCodeListType;
 import sif3.hits.domain.converter.factory.IObjectFactory;
 import sif3.hits.utils.RefIdGenerator;
 
@@ -28,10 +38,95 @@ public class FinancialQuestionnaireCollectionTestData
 
 	public FinancialQuestionnaireCollectionType getTestObject(String refId) {
 		IObjectFactory objectFactory = getObjectFactory();
-		FinancialQuestionnaireCollectionType financialQuestionnaire = new FinancialQuestionnaireCollectionType();
+		FinancialQuestionnaireCollectionType financialQuestionnaire = objectFactory
+				.createFinancialQuestionnaireCollectionType();
 		financialQuestionnaire.setRefId(refId);
-		
+
+		financialQuestionnaire.setFQYear(getDate("2020"));
+		financialQuestionnaire.setRoundCode("RC");
+		financialQuestionnaire.setReportingAuthorityCommonwealthId("RACID");
+		financialQuestionnaire.setSoftwareVendorInfo(getSoftwareVendorInfo("SoftwareProduct", "2.12.0r1234"));
+
+		LocalCodeListType localCodeListType = objectFactory.createLocalCodeListType();
+		for (int i = 1; i < 3; i++) {
+			localCodeListType.getLocalCode().add(getLocalCode(i));
+		}
+		financialQuestionnaire
+				.setLocalCodeList(objectFactory.createAddressCollectionTypeLocalCodeList(localCodeListType));
+
+		FQReportingListType fqReportingListType = objectFactory.createFQReportingListType();
+		for (int i = 1; i < 5; i++) {
+			fqReportingListType.getFQReporting().add(getFQReporting(i));
+		}
+		financialQuestionnaire.setFQReportingList(
+				objectFactory.createFinancialQuestionnaireCollectionTypeFQReportingList(fqReportingListType));
+
 		return financialQuestionnaire;
+	}
+
+	private FQReportingType getFQReporting(int i) {
+		IObjectFactory objectFactory = getObjectFactory();
+		FQReportingType fqReportingType = objectFactory.createFQReportingType();
+		fqReportingType.setEntityLevel("System");
+		fqReportingType
+				.setSchoolInfoRefId(objectFactory.createFQReportingTypeSchoolInfoRefId(SchoolInfoTestData.REF_ID_1));
+		fqReportingType.setLocalId(objectFactory.createFQReportingTypeLocalId("LocalId"));
+		fqReportingType.setStateProvinceId(objectFactory.createFQReportingTypeStateProvinceId("SPID"));
+		fqReportingType.setCommonwealthId(SchoolInfoTestData.LOCAL_ID);
+		fqReportingType.setACARAId(objectFactory.createFQReportingTypeACARAId(SchoolInfoTestData.ACARA_ID));
+		fqReportingType.setEntityName(objectFactory.createFQReportingTypeEntityName(SchoolInfoTestData.SCHOOL_NAME));
+		fqReportingType.setEntityContact(getEntityContact());
+
+		FQContextualQuestionListType fqContextualQuestionListType = objectFactory.createFQContextualQuestionListType();
+		for (int j = 1; j < 6; j++) {
+			fqContextualQuestionListType.getFQContextualQuestion().add(getContextualQuestion(i, j));
+		}
+		fqReportingType.setFQContextualQuestionList(
+				objectFactory.createFQReportingTypeFQContextualQuestionList(fqContextualQuestionListType));
+
+		FQItemListType fqItemListType = objectFactory.createFQItemListType();
+		for (int j = 1; j < 6; j++) {
+			fqItemListType.getFQItem().add(getFQItem(i, j));
+		}
+		fqReportingType.setFQItemList(fqItemListType);
+
+		AGRuleListType agRuleListType = objectFactory.createAGRuleListType();
+		for (int j = 1; j < 6; j++) {
+			agRuleListType.getAGRule().add(getAGRule(i, j));
+		}
+		fqReportingType.setAGRuleList(objectFactory.createFQReportingTypeAGRuleList(agRuleListType));
+		return fqReportingType;
+	}
+
+	private AGRuleType getAGRule(int i, int j) {
+		IObjectFactory objectFactory = getObjectFactory();
+		AGRuleType agRuleType = objectFactory.createAGRuleType();
+		agRuleType.setAGRuleCode(objectFactory.createAGRuleTypeAGRuleCode("Code-" + i + "-" + j));
+		agRuleType.setAGRuleComment(objectFactory.createAGRuleTypeAGRuleComment("Comment + " + i + "-" + j));
+		agRuleType.setAGRuleResponse(objectFactory.createAGRuleTypeAGRuleResponse("Response"));
+		agRuleType.setAGRuleStatus(objectFactory.createAGRuleTypeAGRuleStatus("OK"));
+		return agRuleType;
+	}
+
+	private FQContextualQuestionType getContextualQuestion(int i, int j) {
+		IObjectFactory objectFactory = getObjectFactory();
+		FQContextualQuestionType fqContextualQuestionType = objectFactory.createFQContextualQuestionType();
+		fqContextualQuestionType.setFQContext("Context-" + i + "-" + j);
+		fqContextualQuestionType.setFQAnswer("Answer " + i + " " + j);
+		return fqContextualQuestionType;
+	}
+
+	private FQItemType getFQItem(int i, int j) {
+		IObjectFactory objectFactory = getObjectFactory();
+		FQItemType fqItemType = objectFactory.createFQItemType();
+		fqItemType.setFQItemCode("IC-" + i + j + "." + j);
+		fqItemType.setTuitionAmount(objectFactory.createFQItemTypeTuitionAmount(new BigDecimal("1" + i + j + ".23")));
+		fqItemType.setBoardingAmount(objectFactory.createFQItemTypeBoardingAmount(BigDecimal.ZERO));
+		fqItemType.setSystemAmount(
+				objectFactory.createFQItemTypeSystemAmount(new BigDecimal("" + j + i + "." + i + "0")));
+		fqItemType.setDioceseAmount(objectFactory.createFQItemTypeDioceseAmount(BigDecimal.ZERO));
+		fqItemType.setFQComments(objectFactory.createFQItemTypeFQComments("Comments"));
+		return fqItemType;
 	}
 
 	public String getRefId(int index) {
